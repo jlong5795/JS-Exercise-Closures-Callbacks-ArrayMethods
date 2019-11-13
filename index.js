72327b869a9f14f1bce8736349277cb8edbe95c3
Jason Long <jlong5795@gmail.com>
2019-11-12 18:09:19 -0600

    12 completed tests

--- index.js
+++ index.js
@@ -203,9 +203,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function firstNamesAllCaps(runners) {
+  return runners.map(function(currentValue){
+    return currentValue.first_name.toUpperCase();
+  });
+}//Look at current value. Return the first name of that object in uppercase. Map returns an array for you.
 
 /**
  * ### Challenge `getRunnersByTShirtSize`
